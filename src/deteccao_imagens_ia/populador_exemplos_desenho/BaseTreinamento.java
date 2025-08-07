c183253a509d9e896882f95671c89cfcc99eb43d
Luiz Filipe <luizferamos26@gmail.com>
2025-08-07 17:57:46 -0300

    Refatorado atributos do XML para constantes

--- src/deteccao_imagens_ia/populador_exemplos_desenho/BaseTreinamento.java
+++ src/deteccao_imagens_ia/populador_exemplos_desenho/BaseTreinamento.java
@@ -2,66 +2,70 @@ package deteccao_imagens_ia.populador_exemplos_desenho;
 
 import org.w3c.dom.Element;
 
-import java.awt.*;
+import java.awt.Point;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseTreinamento {
+    public static final String RAIZ_ATTR = "exemplos";
+    public static final String DESENHO_ATTR = "desenho";
+    public static final String HASH_ATTR = "hash";
+    public static final String ROTULO_ATTR = "rotulo";
+    public static final String PONTO_ATTR = "pt";
+    public static final String PONTO_X_ATTR = "x";
+    public static final String PONTO_Y_ATTR = "y";
     private final List<DesenhoClassificado> desenhosClassificados = new ArrayList<>();
 
-    public List<DesenhoClassificado> getDesenhosClassificados() {
-        return desenhosClassificados;
-    }
 
     public void adicionarDesenho(DesenhoClassificado desenho) {
         desenhosClassificados.add(desenho);
     }
 
+    public List<DesenhoClassificado> getDesenhosClassificados() {
+        return  new ArrayList<>(desenhosClassificados);
+    }
+
+    public List<DesenhoClassificado> getEntradasClassificadas() {
+        return new ArrayList<>(desenhosClassificados);
+    }
+
     public void salvarExemplos(File arquivo) throws XMLEditor.FalhaXML {
         var editor = XMLEditor.deArquivo(arquivo);
-        var root = editor.criarAdicionandoElementoRaiz("exemplos");
+        var root = editor.criarAdicionandoElementoRaiz(RAIZ_ATTR);
         for (var desenhoClassificado : desenhosClassificados)
             montarExemplo(editor, root, desenhoClassificado);
         editor.salvar(arquivo);
     }
 
     private void montarExemplo(XMLEditor editor, Element root, DesenhoClassificado desenhoClassificado) {
-        var desenhoElem = editor.criarAdicionandoElemento(root, "desenho");
-        desenhoElem.setAttribute("hash", desenhoClassificado.gerarHash());
-        desenhoElem.setAttribute("rotulo", desenhoClassificado.classificacaoDesenho().paraTexto());
+        var desenhoElem = editor.criarAdicionandoElemento(root, DESENHO_ATTR);
+        desenhoElem.setAttribute(HASH_ATTR, desenhoClassificado.gerarHash());
+        desenhoElem.setAttribute(ROTULO_ATTR, desenhoClassificado.classificacaoDesenho().paraTexto());
 
         for (var ponto : desenhoClassificado.pontosDesenho()) {
-            var pontoElem = editor.criarAdicionandoElemento(desenhoElem, "ponto");
-            pontoElem.setAttribute("x", String.valueOf(ponto.x));
-            pontoElem.setAttribute("y", String.valueOf(ponto.y));
+            var pontoElem = editor.criarAdicionandoElemento(desenhoElem, PONTO_ATTR);
+            pontoElem.setAttribute(PONTO_X_ATTR, String.valueOf(ponto.x));
+            pontoElem.setAttribute(PONTO_Y_ATTR, String.valueOf(ponto.y));
         }
     }
 
     public void carregarExemplos(File arquivo) throws XMLEditor.FalhaXML {
         var editor = XMLEditor.deArquivo(arquivo);
         desenhosClassificados.clear();
-        for (var desenhoElem : editor.obterFilhosIteraveis("desenho"))
+        for (var desenhoElem : editor.obterFilhosIteraveis(DESENHO_ATTR))
             desenhosClassificados.add(lerDesenhoClassificado(editor, desenhoElem));
     }
 
     private DesenhoClassificado lerDesenhoClassificado(XMLEditor editor, Element desenhoElem) {
-        String rotulo = desenhoElem.getAttribute("rotulo");
+        String rotulo = desenhoElem.getAttribute(ROTULO_ATTR);
         var classificacao = ClassificacaoDesenho.deTexto(rotulo);
         var pontos = new ArrayList<Point>();
-        for (var pontoElem : editor.obterFilhosIteraveis(desenhoElem, "ponto")) {
-            int x = Integer.parseInt(pontoElem.getAttribute("x"));
-            int y = Integer.parseInt(pontoElem.getAttribute("y"));
+        for (var pontoElem : editor.obterFilhosIteraveis(desenhoElem, PONTO_ATTR)) {
+            int x = Integer.parseInt(pontoElem.getAttribute(PONTO_X_ATTR));
+            int y = Integer.parseInt(pontoElem.getAttribute(PONTO_Y_ATTR));
             pontos.add(new Point(x, y));
         }
         return new DesenhoClassificado(classificacao, pontos);
-    }
-
-    public List<DesenhoClassificado> obterDesenhosClassificados() {
-        return new ArrayList<>(desenhosClassificados);
-    }
-
-    public List<DesenhoClassificado> obterEntradasClassificadas() {
-        return new ArrayList<>(desenhosClassificados);
     }
 }
